25d82a1b30f7807455dd7b1d93a3ece5c899c694
emersonnobrega <e-nobrega@outlook.com>
2021-05-19 10:32:39 -0300

    subindo projeto

---
+++ src/App.vue
@@ -0,0 +1,78 @@
+<template>
+  <div id="app">
+    <div class="column is-half is-offset-one-quarter">
+      <img id="imgpoke" src="./assets/pokebola.png">
+      <hr>
+      <h4 class="is-size-4">Pokédex</h4>
+      <input type="text" placeholder="Buscar pokemon pelo nome" v-model="busca" class="input is-rounded">
+      <button class="button if-fullwidth is-success" id="buscaBtn" @click="buscar">Buscar</button>
+      <div v-for="(poke,index) in filteredPokemons" :key="poke.url">
+        <Pokemon :name="poke.name" :url="poke.url" :num="index+1"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import Pokemon from './components/Pokemon';
+export default {
+  name: 'App',
+  data(){
+    return{
+      pokemons: [],
+      filteredPokemons: [],
+      busca: ''
+    }
+  },
+  created: function(){
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=251&offset=0").then(res => {
+      console.log("Pegou a lista de pokemons");
+      this.pokemons = res.data.results;
+      this.filteredPokemons = res.data.results;
+    })
+  },
+  components:{
+    Pokemon
+  },
+  methods: {
+    buscar: function(){
+      this.filteredPokemons = this.pokemons;
+      if(this.busca == '' || this.busca == ' '){
+        this.filteredPokemons = this.pokemons;
+      }else{
+        this.filteredPokemons = this.pokemons.filter(pokemon => pokemon.name == this.busca);
+      }
+    }
+  },
+  
+  computed: {/*
+    resultadoBusca: function(){
+      if(this.busca == '' || this.busca == ' '){
+        return this.pokemons;
+      }else{
+        return this.pokemons.filter(pokemon => pokemon.name == this.busca)
+      }
+    }*/
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+#buscaBtn{
+  margin-top: 2%;
+}
+
+#imgpoke{
+  width: 100px
+}
+</style>
